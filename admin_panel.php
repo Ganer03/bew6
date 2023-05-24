9d5ce235e8081fe96383160080449dc81686d5f5
Ganer03 <112845740+Ganer03@users.noreply.github.com>
2023-05-24 19:34:23 +0300

    Update admin_panel.php

--- admin_panel.php
+++ admin_panel.php
@@ -9,7 +9,7 @@
 </head>
 <body>
 <?php
-if ($_SESSION['login']=='ivan'){
+if ($__SESSION['login']=='ivan'){
 $user = 'u52802';
 $pass = '7560818';
 $db = new PDO('mysql:host=localhost;dbname=u52802', $user, $pass, [PDO::ATTR_PERSISTENT => true]);
